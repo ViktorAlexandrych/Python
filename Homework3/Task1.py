4d53d18baba6dd6f9effbd963950f166d76a0b47
Alexandrych <vyrupaev.00@gmail.com>
2023-10-06 23:40:52 +1000

    This commit save +PEP-8

--- Homework3/Task1.py
+++ Homework3/Task1.py
@@ -4,28 +4,24 @@
 # встречаются в обоих наборах.
 # Пользователь вводит 2 числа. n - кол-во элементов первого множества.
 # m - кол-во элементов второго множества. Затем пользователь вводит сами элементы множеств
+from random import randint
 
 n = int(input("Введите количество элементов списка N ", ))
 m = int(input("Введите количество элементов списка M ", ))
-
-from random import randint
 
 list1 = []
 for i in range(n):
     list1.append(randint(0, 20))
 
 list1.sort()  
-print(list1)
-    
-from random import randint
+print("Список N", list1)
 
 list2 = []
 for i in range(m):
     list2.append(randint(0, 20))
 
 list2.sort()
-print(list2)
+print("Список M", list2)
 
 list3 = list(set(list1 + list2))
-
 print(list3)
